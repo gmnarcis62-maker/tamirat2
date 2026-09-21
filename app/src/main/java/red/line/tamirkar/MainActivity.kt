f2d7875bc2174e1ba047dcfa7b2f1c08c2cf2b4e
gmnarcis62-maker <gmnarcis62@gmail.com>
2026-09-21 18:12:23 +0330

    Fix Alignment import error in MainActivity

--- app/src/main/java/red/line/tamirkar/MainActivity.kt
+++ app/src/main/java/red/line/tamirkar/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.text.style.TextAlign
