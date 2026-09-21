ff3eb1cc8f4d433f62582c4f4ea75be6d112e340
gmnarcis62-maker <gmnarcis62@gmail.com>
2026-09-21 17:14:09 +0330

    Debug: simplify Application + remove SQLCipher + remove WorkManager init

--- app/src/main/java/red/line/tamirkar/TamirkarApplication.kt
+++ app/src/main/java/red/line/tamirkar/TamirkarApplication.kt
@@ -1,29 +1,14 @@
 package red.line.tamirkar
 
 import android.app.Application
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import red.line.tamirkar.core.security.SecureLogger
-import timber.log.Timber
-import javax.inject.Inject
 
 @HiltAndroidApp
-class TamirkarApplication : Application(), Configuration.Provider {
-
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
+class TamirkarApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            SecureLogger.plantDebugTree()
-        }
+        SecureLogger.plantDebugTree()
     }
-
-    override val workManagerConfiguration: Configuration
-        get() = Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .setMinimumLoggingLevel(android.util.Log.INFO)
-            .build()
 }
--- app/src/main/java/red/line/tamirkar/data/security/EncryptedDatabaseHelper.kt
+++ app/src/main/java/red/line/tamirkar/data/security/EncryptedDatabaseHelper.kt
@@ -2,7 +2,6 @@ package red.line.tamirkar.data.security
 
 import android.content.Context
 import androidx.room.Room
-import net.zetetic.database.sqlcipher.SupportOpenHelperFactory
 import red.line.tamirkar.data.local.database.RepairDatabase
 import red.line.tamirkar.data.local.seed.SeedData
 
@@ -13,21 +12,11 @@ object EncryptedDatabaseHelper {
 
     fun getDatabase(context: Context, seedData: SeedData): RepairDatabase {
         return INSTANCE ?: synchronized(this) {
-            try {
-                System.loadLibrary("sqlcipher")
-            } catch (e: Throwable) {
-                // ignore
-            }
-
-            val passphrase = "tamirkar_secure_key_2024".toByteArray()
-            val factory = SupportOpenHelperFactory(passphrase)
-
             val db = Room.databaseBuilder(
                 context.applicationContext,
                 RepairDatabase::class.java,
                 "tamirkar.db"
             )
-                .openHelperFactory(factory)
                 .fallbackToDestructiveMigration()
                 .build()
 
